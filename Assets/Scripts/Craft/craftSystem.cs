3f31a1a0837205f0c70a74fb2dab9417b628bf01
EfeTigli <netigli@hotmail.com>
2022-09-10 01:39:10 +0300

    Update CraftSystem Script

--- Assets/Scripts/Craft/craftSystem.cs
+++ Assets/Scripts/Craft/craftSystem.cs
@@ -58,6 +58,10 @@ public class craftSystem : MonoBehaviour
     public Image Image5;
     public Image Image6;
     public Image Image7;
+
+    [Header("Update in Screen Too")]
+    [SerializeField] private craftSystem craftUIScreen;
+    [SerializeField] private bool isScreen;
     void Start(){
         mySaveMaterials.metal = 1200;
         mySaveMaterials.plastic = 600;
@@ -92,33 +96,67 @@ public class craftSystem : MonoBehaviour
             mySaveShip.isLostPlate1 = false;
             plate1CraftButton.enabled = false;
             Image1.enabled = true;
+            if (isScreen)
+            {
+                craftUIScreen.craftPlate1Update();
+            }
         }
         else{
             Debug.Log("You don't have enough metal!!");
         }
     }
+
+    public void craftPlate1Update() 
+    {
+        mySaveShip.isLostPlate1 = false;
+        plate1CraftButton.enabled = false;
+        Image1.enabled = true;
+    }
+
     public void craftPlate2(){
         if(plate2ReqMetal <= mySaveMaterials.metal){
             mySaveMaterials.metal = mySaveMaterials.metal - plate2ReqMetal;
             mySaveShip.isLostPlate2 = false;
             plate2CraftButton.enabled = false;
             Image2.enabled = true;
+            if (isScreen)
+            {
+                craftUIScreen.craftPlate2Update();
+            }
         }
         else{
             Debug.Log("You don't have enough metal!!");
         }
     }
+
+    public void craftPlate2Update()
+    {
+        mySaveShip.isLostPlate2 = false;
+        plate2CraftButton.enabled = false;
+        Image2.enabled = true;
+    }
+
     public void craftPlastic(){
         if(oilSlider.value * oilPerPlastic <= mySaveMaterials.oil){
             mySaveMaterials.oil = mySaveMaterials.oil - (oilSlider.value * oilPerPlastic);
             mySaveMaterials.plastic = mySaveMaterials.plastic + oilSlider.value;
             oilSlider.SetValueWithoutNotify(0);
+            if (isScreen)
+            {
+                craftUIScreen.craftPlasticUpdate();
+            }
         }
         else{
             Debug.Log("You don't have enough oil!!");
             oilSlider.SetValueWithoutNotify(0);
         }
     }
+
+    public void craftPlasticUpdate()
+    {
+        oilSlider.SetValueWithoutNotify(0);
+    }
+
     public void upgradeMP(){
         if(mPReqMetal <= mySaveMaterials.metal){
             mySaveMaterials.metal = mySaveMaterials.metal - mPReqMetal;
@@ -126,11 +164,22 @@ public class craftSystem : MonoBehaviour
             myPickAxe.AssignPickaxeHead();
             mPUpgradeButton.enabled = false;
             Image3.enabled = true;
+            if (isScreen)
+            {
+                craftUIScreen.upgradeMPUpdate();
+            }
         }
         else{
             Debug.Log("You don't have enough metal!!");
         }
     }
+
+    public void upgradeMPUpdate()
+    {
+        mPUpgradeButton.enabled = false;
+        Image3.enabled = true;
+    }
+
     public void upgradeGP(){
         if(gPReqGold <= mySaveMaterials.gold){
             mySaveMaterials.metal = mySaveMaterials.metal - mPReqMetal;
@@ -138,11 +187,22 @@ public class craftSystem : MonoBehaviour
             myPickAxe.AssignPickaxeHead();
             gPUpgradeButton.enabled = false;
             Image4.enabled = true;
+            if (isScreen)
+            {
+                craftUIScreen.upgradeGPUpdate();
+            }
         }
         else{
             Debug.Log("You don't have enough gold!!");
         }
     }
+
+    public void upgradeGPUpdate()
+    {
+        mPUpgradeButton.enabled = false;
+        Image3.enabled = true;
+    }
+
     public void UpgradeOSL(){
         if(mySaveMaterials.oxygenSystemLevel == 1){
             if(oS2ReqMetal <= mySaveMaterials.metal && oS2ReqPlastic <= mySaveMaterials.plastic){
@@ -155,6 +215,10 @@ public class craftSystem : MonoBehaviour
                 osPlastic2.enabled = true;
                 oSMetal2.enabled = true;
                 oSLevel3.enabled = true;
+                if (isScreen)
+                {
+                    craftUIScreen.UpgradeOSL1Update();
+                }
             }
             else if(oS2ReqMetal <= mySaveMaterials.metal){
                 Debug.Log("You don't have enough plastic!!");
@@ -174,6 +238,10 @@ public class craftSystem : MonoBehaviour
                 oSUpgradeButton.enabled = false;
                 Image5.enabled = true;
                 Image6.enabled = true;
+                if (isScreen)
+                {
+                    craftUIScreen.UpgradeOSL2Update();
+                }
             }
             else if(oS3ReqMetal <= mySaveMaterials.metal){
                 Debug.Log("You don't have enough plastic!!");
@@ -186,6 +254,24 @@ public class craftSystem : MonoBehaviour
             }
         }
     }
+
+    public void UpgradeOSL1Update()
+    {
+        osPlastic1.enabled = false;
+        oSMetal1.enabled = false;
+        oSLevel2.enabled = false;
+        osPlastic2.enabled = true;
+        oSMetal2.enabled = true;
+        oSLevel3.enabled = true;
+    }
+
+    public void UpgradeOSL2Update()
+    {
+        oSUpgradeButton.enabled = false;
+        Image5.enabled = true;
+        Image6.enabled = true;
+    }
+
     public void UpgradeBL(){
         if(mySaveMaterials.bootsLevel == 1){
             if(bL2ReqPlastic <= mySaveMaterials.plastic){
@@ -195,6 +281,10 @@ public class craftSystem : MonoBehaviour
                 bLLevel3.enabled = true;
                 bLPlastic1.enabled = false;
                 bLPlastic2.enabled = true;
+                if (isScreen)
+                {
+                    craftUIScreen.UpgradeBL1Update();
+                }
             }
             else{
                 Debug.Log("You don't have enough plastic!!");
@@ -206,10 +296,28 @@ public class craftSystem : MonoBehaviour
                 mySaveMaterials.bootsLevel = 3;
                 bLUpgradeButton.enabled = false;
                 Image7.enabled = true;
+                if (isScreen)
+                {
+                    craftUIScreen.UpgradeBL2Update();
+                }
             }
             else{
                 Debug.Log("You don't have enough plastic!!");
             }
         }
+    }
+
+    public void UpgradeBL1Update()
+    {
+        bLLevel2.enabled = false;
+        bLLevel3.enabled = true;
+        bLPlastic1.enabled = false;
+        bLPlastic2.enabled = true;
+    }
+
+    public void UpgradeBL2Update()
+    {
+        bLUpgradeButton.enabled = false;
+        Image7.enabled = true;
     }
 }
