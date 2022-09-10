21ee432abfe2301692178e80a7575ace4b1231b4
EfeTigli <netigli@hotmail.com>
2022-09-10 15:16:52 +0300

    Create CursorManager Script

---
+++ Assets/Scripts/Manager/CursorManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    [SerializeField] private bool InvisibleInBeginning;
+
+    private void Awake()
+    {
+        if (InvisibleInBeginning)
+            LockCursor();
+        else if (!InvisibleInBeginning)
+            UnlockCursor();
+    }
+
+    public void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void UnlockCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
