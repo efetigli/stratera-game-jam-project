bad53b981ad91bdc4b456100e27f2ff2658dd75d
EfeTigli <netigli@hotmail.com>
2022-09-10 15:18:52 +0300

    Update RichOxygen Script

--- Assets/Scripts/RichOxygenArea.cs
+++ Assets/Scripts/RichOxygenArea.cs
@@ -6,13 +6,29 @@ public class RichOxygenArea : MonoBehaviour
 {
     public bool isInsideOxygenRichArea { get; private set; }
 
+    [Header("Player Flaslight")]
+    [SerializeField] private GameObject flashLight;
+
+    private void Update()
+    {
+        Debug.Log(isInsideOxygenRichArea);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            isInsideOxygenRichArea = true;
+            flashLight.SetActive(false);
+        }
+    }
 
     private void OnTriggerStay(Collider other)
     {
         // 8 is Player layer.
-        if(other.gameObject.layer == 8)
+        if (other.gameObject.layer == 8)
         {
             isInsideOxygenRichArea = true;
+            flashLight.SetActive(false);
         }
     }
 
@@ -21,6 +37,7 @@ public class RichOxygenArea : MonoBehaviour
         if (other.gameObject.layer == 8)
         {
             isInsideOxygenRichArea = false;
+            flashLight.SetActive(true);
         }
     }
 }
