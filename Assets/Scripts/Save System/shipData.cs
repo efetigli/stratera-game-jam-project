b7fabd2ac7b1798467c62095bdfce597bca972e3
EfeTigli <netigli@hotmail.com>
2022-09-10 19:43:44 +0300

    Update Ship Data

--- Assets/Scripts/Save System/shipData.cs
+++ Assets/Scripts/Save System/shipData.cs
@@ -4,14 +4,19 @@ public class shipData
     public float speed;
     public float durability;
     public float aerodynamics;
+    public bool hasLostPlate1;
+    public bool hasLostPlate2;
     public bool isLostPlate1;
     public bool isLostPlate2;
     public float wingLevel;
-    public float wingPieceLevel;
+    public bool hasWingPieceLevel2;
+    public bool hasWingPieceLevel3;
     public float engineLevel;
-    public float enginePiecesLevel;
+    public bool hasEngineLevel2;
+    public bool hasEngineLevel3;
     public float boosterRocketLevel;
-    public float boosterRocketPiecesLevel;
+    public bool hasBoosterRocketLevel2;
+    public bool hasBoosterRocketLevel3;
     public float oilTankLevel;
     public float oilTankPiecesLevel;
     
@@ -22,12 +27,17 @@ public class shipData
     aerodynamics = ship.aerodynamics;
     isLostPlate1 = ship.isLostPlate1;
     isLostPlate2 = ship.isLostPlate2;
+    hasLostPlate1 = ship.hasLostPlate1;
+    hasLostPlate2 = ship.hasLostPlate2;
     wingLevel = ship.wingLevel;
-    wingPieceLevel = ship.wingPieceLevel;
+    hasWingPieceLevel2 = ship.hasWingPieceLevel2;
+    hasWingPieceLevel3 = ship.hasWingPieceLevel3;
     engineLevel = ship.engineLevel;
-    enginePiecesLevel = ship.enginePiecesLevel;
+        hasEngineLevel2 = ship.hasEngineLevel2;
+        hasEngineLevel3 = ship.hasEngineLevel3;
     boosterRocketLevel = ship.boosterRocketLevel;
-    boosterRocketPiecesLevel = ship.boosterRocketPiecesLevel;
+        hasBoosterRocketLevel3 = ship.hasBoosterRocketLevel3;
+        hasBoosterRocketLevel3 = ship.hasBoosterRocketLevel3;
     oilTankLevel = ship.oilTankLevel;
     oilTankPiecesLevel = ship.oilTankPiecesLevel;
     }   
